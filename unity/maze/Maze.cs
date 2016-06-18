7ead4cbb2ee366b3b67537ffd83deb18aac3773e
Roberto De Ioris <roberto@20tab.com>
2016-06-18 16:47:34 +0200

    added maze example

---
+++ unity/maze/Maze.cs
@@ -0,0 +1,221 @@
+﻿using System;
+
+
+public class Maze
+{
+
+    public class Cell
+    {
+        public bool wayNorth;
+        public bool waySouth;
+        public bool wayEast;
+        public bool wayWest;
+        public bool processed;
+    }
+
+    private Cell[] cells;
+
+    private int width;
+    private int height;
+
+	private delegate void CardinalPoint(int x, int y);
+	private CardinalPoint[] cardinalPoints;
+
+
+    public int Width
+    {
+        get
+        {
+            return this.width;
+        }
+    }
+
+    public int Height
+    {
+        get
+        {
+            return this.height;
+        }
+    }
+
+    private Random rand;
+
+    public Maze(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+
+		// fill the cells array
+        this.cells = new Cell[width * height];
+        for (int i = 0; i < (width * height); i++)
+        {
+            this.cells[i] = new Cell();
+        }
+
+		this.cardinalPoints = new CardinalPoint[] {
+			this.ManageNorth,
+			this.ManageSouth,
+			this.ManageEast,
+			this.ManageWest
+		};
+
+		this.rand = new System.Random();
+
+        this.RandomCarve();
+    }
+
+    public Cell GetCell(int x, int y)
+    {
+        if (x < 0 || x > this.width - 1)
+            throw new Exception("Invalid x coordinate for Maze cell");
+
+        if (y < 0 || y > this.height - 1)
+            throw new Exception("Invalid y coordinate for Maze cell");
+
+        int pos = (y * this.width) + x;
+
+        return this.cells[pos];
+    }
+
+	// choose a random starting cell to carve
+    private void RandomCarve()
+    {
+        int randomCell = this.rand.Next(this.cells.Length);
+        int x = randomCell % this.width;
+        int y = randomCell / this.width;
+
+        this.ManageCell(x, y);
+    }
+
+    private void ManageCell(int x, int y)
+    {
+
+		// shuffle the cardinal points array of funcs
+		for (int i = 0; i < cardinalPoints.Length; i++)
+        {
+			int randomPos = this.rand.Next(i, cardinalPoints.Length);
+			CardinalPoint tmpPoint = cardinalPoints[randomPos];
+			cardinalPoints[randomPos] = cardinalPoints[i];
+			cardinalPoints[i] = tmpPoint;
+        }
+
+		// call each of the cardinal point funcs
+		foreach(CardinalPoint pointFunc in cardinalPoints)
+			pointFunc(x, y);
+
+    }
+
+	// mark a cell as processed
+    private void MarkCell(int x, int y)
+    {
+        int pos = (y * this.width) + x;
+
+        this.cells[pos].processed = true;
+    }
+
+	// carve the north wall
+    private void OpenNorth(int x, int y)
+    {
+        int pos = (y * this.width) + x;
+
+        this.cells[pos].wayNorth = true;
+    }
+
+	// carve the south wall
+    private void OpenSouth(int x, int y)
+    {
+        int pos = (y * this.width) + x;
+
+        this.cells[pos].waySouth = true;
+    }
+
+	// carve the east wall
+    private void OpenEast(int x, int y)
+    {
+        int pos = (y * this.width) + x;
+
+        this.cells[pos].wayEast = true;
+    }
+
+	// carve the west wall
+    private void OpenWest(int x, int y)
+    {
+        int pos = (y * this.width) + x;
+
+        this.cells[pos].wayWest = true;
+    }
+
+
+    private void ManageNorth(int x, int y)
+    {
+        if (y < 1)
+            return;
+
+        Cell northCell = GetCell(x, y - 1);
+
+        if (northCell.processed)
+            return;
+
+        this.OpenNorth(x, y);
+        // set cell as processed
+        this.MarkCell(x, y);
+        this.OpenSouth(x, y - 1);
+
+        this.ManageCell(x, y - 1);
+    }
+
+    private void ManageSouth(int x, int y)
+    {
+        if (y >= this.height - 1)
+            return;
+
+        Cell southCell = this.GetCell(x, y + 1);
+
+        if (southCell.processed)
+            return;
+
+        this.OpenSouth(x, y);
+        // set cell as processed
+        this.MarkCell(x, y);
+		this.OpenNorth(x, y + 1);
+
+        this.ManageCell(x, y + 1);
+	}
+
+    private void ManageEast(int x, int y)
+    {
+        if (x >= this.width - 1)
+            return;
+
+		Cell eastCell = this.GetCell(x + 1, y);
+
+        if (eastCell.processed)
+            return;
+
+        this.OpenEast(x, y);
+        // set cell as processed
+        this.MarkCell(x, y);
+        this.OpenWest(x + 1, y);
+
+        this.ManageCell(x + 1, y);
+    }
+
+    private void ManageWest(int x, int y)
+    {
+        if (x < 1)
+            return;
+
+        Cell westCell = this.GetCell(x - 1, y);
+
+        if (westCell.processed)
+            return;
+
+        this.OpenWest(x, y);
+        // set cell as processed
+        this.MarkCell(x, y);
+        this.OpenEast(x - 1, y);
+
+        this.ManageCell(x - 1, y);
+    }
+
+}
---
+++ unity/maze/MazeGenerator.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeGenerator : MonoBehaviour {
+
+	private Maze maze;
+
+	private const float planeBaseSize = 10;
+
+	public int mazeWidth = 10;
+	public int mazeHeight = 10;
+
+	public float wallSize = 1;
+	public float wallHeight = 1;
+
+	public float wallThin = 0.05f;
+
+	public Material[] wallsMaterials;
+
+	private GameObject mazeRoot;
+
+
+	// Use this for initialization
+	void Start() {
+		maze = new Maze(mazeWidth, mazeHeight);
+
+		mazeRoot = new GameObject("Maze");
+
+		DrawMaze();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	private void DrawHorizontalWall(float x, float y, Material material = null) {
+		GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		wall.transform.localScale = new Vector3(wallSize, wallHeight, wallThin);
+		wall.transform.position = new Vector3(x, wallHeight / 2, y);
+		wall.name = "Horizontal Wall at " + x + "/" + y;
+		if (material)
+			wall.GetComponent<Renderer>().material = material;
+		wall.transform.SetParent(mazeRoot.transform);
+	}
+
+	private void DrawVerticalWall(float x, float y, Material material = null) {
+		GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		wall.transform.localScale = new Vector3(wallThin, wallHeight, wallSize);
+		wall.transform.position = new Vector3(x, wallHeight / 2, y);
+		wall.name = "Vertical Wall at " + x + "/" + y;
+		if (material)
+			wall.GetComponent<Renderer>().material = material;
+		wall.transform.SetParent(mazeRoot.transform);
+	}
+
+	private void DrawMaze() {
+		for (float y = 0; y < maze.Height; y++) {
+			for (float x = 0; x < maze.Width; x++) {
+               
+				Maze.Cell cell = maze.GetCell((int)x, (int)y);
+
+				if (!cell.wayNorth && y == 0) {
+					float wallX = x * wallSize - maze.Width * wallSize / 2f + wallSize / 2f;
+					float wallY = -y * wallSize + maze.Height * wallSize / 2f - wallThin / 2f;
+					this.DrawHorizontalWall(wallX, wallY, GetRandomMaterial());
+				}
+
+				if (!cell.waySouth) {
+					float wallX = x * wallSize - maze.Width * wallSize / 2f + wallSize / 2f;
+					float wallY = -y * wallSize + maze.Height * wallSize / 2f - wallSize + wallThin / 2f;
+					this.DrawHorizontalWall(wallX, wallY, GetRandomMaterial());
+				}
+               
+				if (!cell.wayWest && x == 0) {
+					float wallX = x * wallSize - maze.Width * wallSize / 2f + wallThin / 2f;
+					float wallY = -y * wallSize + maze.Height * wallSize / 2f - wallSize / 2f;
+					this.DrawVerticalWall(wallX, wallY, GetRandomMaterial());
+				}
+
+				if (!cell.wayEast) {
+					float wallX = x * wallSize - maze.Width * wallSize / 2f + wallSize - wallThin / 2f;
+					float wallY = -y * wallSize + maze.Height * wallSize / 2f - wallSize / 2f;
+					this.DrawVerticalWall(wallX, wallY, GetRandomMaterial());
+				}
+
+
+			}
+		}
+	}
+
+	private Material GetRandomMaterial() {
+		if (wallsMaterials.Length == 0)
+			return null;
+		return wallsMaterials [Random.Range(0, wallsMaterials.Length)];
+	}
+}
